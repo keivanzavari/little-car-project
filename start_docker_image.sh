9bc6cacf1e47607fe245fc2607dc556a34b09bfb
Keivan Zavari <keivan@ivex.ai>
2019-06-20 18:06:34 +0200

    Fix path mount projects

--- start_docker_image.sh
+++ start_docker_image.sh
@@ -3,7 +3,7 @@
 IMAGE_TAG=donkey_car_project
 docker run --rm -it \
    --env="DISPLAY" \
-   --workdir=/app \
-   --volume="$PWD":/app \
+   --workdir=/projects \
+   --volume="$PWD":/projects/little_cart_project \
    ${IMAGE_TAG} \
    /bin/zsh
